806daf9aadbdf88bb3f136e02c109848fef4ff21
ayecmon <112906516+ayecmon@users.noreply.github.com>
2026-01-14 20:40:49 -0800

    Create dear_voice project

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import './HomePage.css';
+export function HomePage(){
+    return(
+        <>
+        <h2>What's Happening ?</h2>
+        <div className="button-container">
+            <button>Police (ရဲ)</button>
+            <button>Medical Emergency (ဆေးဘက်ဆိုင်ရာအရေးပေါ်အခြေအနေ)</button>
+            <button>Immigration (လူဝင်မှုကြီးကြပ်ရေး)</button>
+        </div>
+        
+        </>
+    );
+}
