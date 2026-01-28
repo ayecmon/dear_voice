2842ed2aea2d18f3f26056c670f93a5ea56caff5
ayecmon <112906516+ayecmon@users.noreply.github.com>
2026-01-27 21:20:28 -0800

    Add ConversationTabs in components

---
+++ src/components/ConversationTabs.jsx
@@ -0,0 +1,16 @@
+
+import "./ConversationTabs.css";
+
+export function ConversationTabs() {
+  return (
+    <>
+      <p className="warning-label">အရေးပေါ်အခြေအနေရှိပါက 911 သို့ခေါ်ဆိုပါ။ (If there is an emergency situation, Please call 911)</p>
+      <div className="button-flex-container">
+        <button className="button-options">စကားပြော Conversation</button>
+        <button className="button-options">
+          အသုံးများသော စကားစုများ Phrases
+        </button>
+      </div>
+    </>
+  );
+}
