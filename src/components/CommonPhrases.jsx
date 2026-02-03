d0c06eb70650e6fe50e7ff93762f7889397fa097
ayecmon <112906516+ayecmon@users.noreply.github.com>
2026-02-02 20:25:53 -0800

    Create CommonPhrases component

---
+++ src/components/CommonPhrases.jsx
@@ -0,0 +1,13 @@
+import './CommonPhrases.css';
+export function CommonPhrases() {
+  return (
+    <>
+      <p>အသုံးများသော စကားစုများ</p>
+      <div className="phrases-button">
+        <button className="phrase"></button>
+        <button className="phrase"></button>
+        <button className="phrase"></button>
+      </div>
+    </>
+  );
+}
